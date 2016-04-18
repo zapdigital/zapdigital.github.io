da0cc3a033323889d35bcae785bcddffa4e8a188
Waveharp <Waveharp@users.noreply.github.com>
2016-04-18 08:14:43 -0500

    Add Gulp for static asset management

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,36 @@
-var gulp 				= require('gulp'),
-    postcss 		= require('gulp-postcss'),
-    sourcemaps  = require('gulp-sourcemaps'),
-    lost 				= require('lost'),
-    rucksack    = require('rucksack-css');
+var gulp 				 = require('gulp');
+var postcss 		 = require('gulp-postcss');
+var sourcemaps   = require('gulp-sourcemaps');
+var precss       = require('precss');
+var lost 				 = require('lost');
+var rucksack     = require('rucksack-css');
+var flexboxFixes = require('postcss-flexbugs-fixes');
+var imagemin		 = require('gulp-imagemin');
+var slim 				 = require('gulp-slim');
+var ghPages 		 = require('gulp-gh-pages');
 
-var browserSync = require('browser-sync');
-var reload 			= browserSync.reload;
+var browserSync  = require('browser-sync');
+var reload 			 = browserSync.reload;
 
 // Styles - PostCSS, Lost, Rucksack, sourcemaps
-gulp.task('styles', function() {
+gulp.task('styles', function () {
 	return gulp.src('./src/css/*.css')
 		.pipe(sourcemaps.init())
 		.pipe(postcss([
+			precss(),
 			lost(),
 			rucksack({
 				autoprefixer: true
-			})
+			}),
+			flexboxFixes()
 		]))
-		.pipe(sourcemaps.write('./'))
+		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('./dist/css'))
 		.pipe(reload({stream: true}));
 });
 
 // Start static server and watch files
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
 	browserSync({
 		server: {
 			baseDir: './dist'
@@ -32,14 +39,49 @@ gulp.task('browser-sync', function() {
 });
 
 // Watch
-gulp.task('watch', function() {
+gulp.task('watch', function () {
 
 	// Watch css files
 	gulp.watch('./src/**/*.css', ['styles', reload]);
 
-	// Watch html files
-	gulp.watch('dist/*.html', reload);
+	// Watch slim files
+	gulp.watch('./src/views/*.slim', ['markup', reload]);
 
+	// Watch image files
+	gulp.watch('./src/img/*', ['images', reload]);
 });
 
-gulp.task('default', ['browser-sync', 'watch', 'styles']);
+// Images
+gulp.task('images', function () {
+	return gulp.src('src/img/*')
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}]
+		}))
+		.pipe(gulp.dest('dist/img'));
+});
+
+// Minify
+gulp.task('markup', function () {
+	return gulp.src('src/views/*.slim')
+		.pipe(slim({
+			require: 'slim/include',
+			options: 'include_dirs=["./src/views/includes"]'
+    }))
+    .pipe(gulp.dest('dist/'));
+});
+
+// Build
+gulp.task('build', ['styles', 'markup', 'images']);
+
+// Deploy
+gulp.task('deploy', ['build'], function () {
+	return gulp.src('./dist/**/*')
+		.pipe(ghPages({
+				branch: 'master',
+				force: true
+			}))
+})
+
+// Default
+gulp.task('default', ['browser-sync', 'watch', 'styles', 'images', 'markup']);
--- readme.md
+++ readme.md
@@ -1,7 +1,16 @@
 # Usage
-```npm install``` to install dependencies  
-`gulp` to start a Live Reload enabled Browser-Sync server  
-`gulp styles` to build CSS  
+```npm install``` to install dependencies
+`gulp` to start a Live Reload enabled Browser-Sync server
+`gulp build` to build static assets
+`gulp deploy` to build static assets & upload them to GH Pages
+
+## Slim
+We need Slim in version 3.0.2 or greater. If you don't have Slim installed, please install Ruby (and RubyGems) first and run
+`gem install slim -v '>= 3.0.2'`
+
+If you already have Slim installed, make sure you are using the latest version:
+`gem update slim`
+
 
 This framework is using:
 - [PostCSS](https://github.com/postcss/postcss)
@@ -11,6 +20,7 @@ This framework is using:
 - [Browser-Sync](https://www.browsersync.io/)
 
 ## To Do
-- [ ] move HTML, images to src folder
-- [ ] add minification to build process
-- [ ] optimize images
+- [x] move HTML, images to src folder
+- [x] add minification to build process
+- [x] optimize images
+- [x] minify css
